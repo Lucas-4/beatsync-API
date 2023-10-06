561122be63b742bc5d47b3f9865bddcba4dcae8e
unknown <diaslucas8822@gmail.com>
2023-10-06 19:04:39 -0300

    fixed db config for production

--- src/db/db.js
+++ src/db/db.js
@@ -1,11 +1,17 @@
 const mysql = require("mysql2");
 
-const db = mysql.createPool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
-  dateStrings: process.env.DB_DATE_STRINGS,
-});
+let db;
+
+if (process.env.NODE_ENV === "development") {
+  db = mysql.createPool({
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    dateStrings: process.env.DB_DATE_STRINGS,
+  });
+} else {
+  db = mysql.createPool(process.env.CLEARDB_DATABASE_URL);
+}
 
 module.exports = db.promise();
